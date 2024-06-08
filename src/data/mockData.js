dbd605b3c09373a0f0fa22130d211c72444d3b6f
unknown <gilliedudik@gmail.com>
2024-06-08 18:29:10 +0300

    added data

---
+++ src/data/mockData.js
@@ -0,0 +1,138 @@
+export const mockDataList = [
+        {
+            id:1,
+            name:"Maxie",
+            email:"mhadkins0@yahoo.co.jp",
+            age:51,
+            phone:"627-191-9726",
+            job_title:"Engineer II"
+        }, {
+            id:2,
+            name:"Leeann",
+            email:"lsutcliff1@ca.gov",
+            age:52,
+            phone:"920-668-7060",
+            job_title:"Assistant Manager"
+        }, {
+            id:3,
+            name:"Ermina",
+            email:"epenella2@accuweather.com",
+            age:32,
+            phone:"809-544-3787",
+            job_title:"Compensation Analyst"
+        }, {
+            id:4,
+            name:"Lonni",
+            email:"lpiele3@cornell.edu",
+            age:4,
+            phone:"948-804-5355",
+            job_title:"Associate Professor"
+        }, {
+            id:5,
+            name:"Noellyn",
+            email:"ncrann4@usnews.com",
+            age:85,
+            phone:"590-345-4533",
+            job_title:"Administrative Assistant II"
+        }, {
+            id:6,
+            name:"Jandy",
+            email:"jbellow5@mashable.com",
+            age:26,
+            phone:"866-317-4203",
+            job_title:"Teacher"
+        }, {
+            id:7,
+            name:"Philippe",
+            email:"pnetherwood6@hud.gov",
+            age:37,
+            phone:"509-996-0306",
+            job_title:"Web Designer III"
+        }, {
+            id:8,
+            name:"Hulda",
+            email:"hvernazza7@hubpages.com",
+            age:28,
+            phone:"722-197-0229",
+            job_title:"Research Assistant III"
+        }, {
+            id:9,
+            name:"Gussi",
+            email:"gdensie8@bigcartel.com",
+            age:39,
+            phone:"558-119-7913",
+            job_title:"Staff Scientist"
+        }, {
+            id:10,
+            name:"Cleavland",
+            email:"cgiblin9@bandcamp.com",
+            age:18,
+            phone:"558-508-8038",
+            job_title:"Nuclear Power Engineer"
+        }, {
+            id:11,
+            name:"Joete",
+            email:"jverbruggena@xinhuanet.com",
+            age:19,
+            phone:"371-519-0591",
+            job_title:"Account Coordinator"
+        }, {
+            id:12,
+            name:"Robbie",
+            email:"rlodevickb@lulu.com",
+            age:26,
+            phone:"823-802-1678",
+            job_title:"Systems Administrator III"
+        }, {
+            "id":13,"name":"Dare","email":"dpreddyc@clickbank.net","age":13,"phone":"813-907-7158","job_title":"Assistant Professor"},
+        {
+            id:14,
+            name:"Koren",
+            email:"khendricksond@bizjournals.com",
+            age:44,
+            phone:"410-471-3772",
+            job_title:"Technical Writer"
+        }, {
+            id:15,
+            name:"Luce",
+            email:"lromanettie@alexa.com",
+            age:25,
+            phone:"917-298-5495",
+            job_title:"Community Outreach Specialist"
+        }, {
+            id:16,
+            name:"Harrietta",
+            email:"hclemmensf@ox.ac.uk",
+            age:16,
+            phone:"282-929-7586",
+            job_title:"Administrative Officer"
+        }, {
+            id:17,
+            name:"Giselle",
+            email:"gsallansg@tiny.cc",
+            age:37,
+            phone:"337-246-0985",
+            job_title:"Programmer Analyst II"
+        },{
+            id:18,
+            name:"Heather",
+            email:"hfarnishh@walmart.com",
+            age:38,
+            phone:"237-234-5594",
+            job_title:"Information Systems Manager"
+        }, {
+            id:19,
+            name:"Vincent",
+            email:"vbrookhousei@vkontakte.ru",
+            age:49,
+            phone:"903-353-2827",
+            job_title:"Structural Engineer"
+        }, {
+            id:20,
+            name:"Dewain",
+            email:"dgatheridgej@comsenz.com",
+            age:29,
+            phone:"489-636-7331",
+            job_title:"Marketing Assistant"
+        }
+]
